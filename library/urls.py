e66bcf86f1fd3e9350167cab7166de51b7d7860d
kmangesh49 <kumarmangesh55@gmail.com>
2020-03-23 12:34:15 +0530

    added templates

--- library/urls.py
+++ library/urls.py
@@ -2,6 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path("",views.index,name="index")
+    path("",views.index,name="index"),
+    path("register/",views.register,name="register")
 ]
 
--- library/views.py
+++ library/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render,reverse
+from django.http import HttpResponse,HttpResponseRedirect
+from .models import Author
 # Create your views here.
 def index(request):
-    return HttpResponse("hello world")
+    context={
+        "authors":Author.objects.all()
+    }
+    return render(request,"library/index.html",context=context)
+
+
+def register(request):
+    context = {
+        "message":"Hello"
+    }
+    if request.method == "POST":
+        name=request.POST.get("name")
+        branch=request.POST.get("branch")
+        year=request.POST.get("year")
+        email=request.POST.get("email")
+        password=request.POST.get("password")
+        confirm_password=request.POST.get("confirm password")
+        if not name:
+            context["message"] = "provide a valid name"
+            return render(request,"library/register.html",context)    
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request,"library/register.html",context)    
